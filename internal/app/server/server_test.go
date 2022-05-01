ce6198d48c1d4ea337a6364a176453043e563d69
Aleksandra Obriadina <xbreathoflife@gmail.com>
2022-05-01 21:18:22 +0300

    get orders test

--- internal/app/server/server_test.go
+++ internal/app/server/server_test.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/xbreathoflife/gophermart/internal/app/entities"
 	"github.com/xbreathoflife/gophermart/internal/app/storage/mocks"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -25,54 +27,53 @@ func TestServer_RegisterLogin(t *testing.T) {
 		want     want
 	}{
 		{
-			name: "simple register",
+			name:     "simple register",
 			userData: entities.LoginRequest{Login: "hello", Password: "there"},
-			target: "/api/user/register",
+			target:   "/api/user/register",
 			want: want{
 				statusCode: 200,
 			},
 		},
 		{
-			name: "register bad request",
+			name:     "register bad request",
 			userData: entities.LoginRequest{Login: "hello"},
-			target: "/api/user/register",
+			target:   "/api/user/register",
 			want: want{
 				statusCode: 400,
 			},
 		},
 		{
-			name: "register username taken",
+			name:     "register username taken",
 			userData: entities.LoginRequest{Login: "goodbye", Password: "there"},
-			target: "/api/user/register",
+			target:   "/api/user/register",
 			want: want{
 				statusCode: 409,
 			},
 		},
 		{
-			name: "simple login",
+			name:     "simple login",
 			userData: entities.LoginRequest{Login: "goodbye", Password: "123456"},
-			target: "/api/user/login",
+			target:   "/api/user/login",
 			want: want{
 				statusCode: 200,
 			},
 		},
 		{
-			name: "login bad request",
+			name:     "login bad request",
 			userData: entities.LoginRequest{Login: "goodbye"},
-			target: "/api/user/login",
+			target:   "/api/user/login",
 			want: want{
 				statusCode: 400,
 			},
 		},
 		{
-			name: "login unauthorized",
+			name:     "login unauthorized",
 			userData: entities.LoginRequest{Login: "goodbye", Password: "34"},
-			target: "/api/user/login",
+			target:   "/api/user/login",
 			want: want{
 				statusCode: 401,
 			},
 		},
-
 	}
 
 	mockCtrl := gomock.NewController(t)
@@ -157,19 +158,23 @@ func TestServer_InsertNewOrder(t *testing.T) {
 		&entities.UserModel{Login: "hello", PasswordHash: "123456", Session: "123"}, nil).MinTimes(0)
 	repo.EXPECT().UpdateUserSession(gomock.Any(), gomock.Any()).MinTimes(0)
 	repo.EXPECT().GetUserBySessionIfExists(gomock.Any(), gomock.Any()).Return(
-		&entities.UserSessionModel{Login: "hello",  Session: "123"}, nil).MinTimes(0)
+		&entities.UserSessionModel{Login: "hello", Session: "123"}, nil).MinTimes(0)
 	repo.EXPECT().GetOrderIfExists(gomock.Any(), "2377225624").Return(nil, nil).MinTimes(0)
+
+	orderTime, err := time.Parse(time.RFC3339, "2022-04-30T20:00:00+03:00")
+	require.NoError(t, err)
+
 	repo.EXPECT().GetOrderIfExists(gomock.Any(), "12345678903").Return(
 		&entities.OrderModel{OrderNum: "12345678903",
-			Login: "hello",
-			UploadedAt: time.Date(2022, 4, 21, 1, 10, 30, 0, time.UTC),
-			Status: "NEW",
+			Login:      "hello",
+			UploadedAt: orderTime,
+			Status:     "NEW",
 		}, nil).MinTimes(0)
 	repo.EXPECT().GetOrderIfExists(gomock.Any(), "562246784655").Return(
 		&entities.OrderModel{OrderNum: "562246784655",
-			Login: "goodbye",
-			UploadedAt: time.Date(2022, 4, 21, 1, 10, 30, 0, time.UTC),
-			Status: "NEW",
+			Login:      "goodbye",
+			UploadedAt: orderTime,
+			Status:     "NEW",
 		}, nil).MinTimes(0)
 	repo.EXPECT().InsertNewOrder(gomock.Any(), gomock.Any()).MinTimes(0)
 
@@ -181,12 +186,14 @@ func TestServer_InsertNewOrder(t *testing.T) {
 	h := server.ServerHandler()
 	h.ServeHTTP(w, request)
 	result := w.Result()
-
 	cookie := http.Cookie{}
 	if len(result.Cookies()) > 0 {
 		auth := result.Cookies()[0]
 		cookie = http.Cookie{Name: auth.Name, Value: auth.Value}
 	}
+
+	err = result.Body.Close()
+	require.NoError(t, err)
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -204,4 +211,99 @@ func TestServer_InsertNewOrder(t *testing.T) {
 			require.NoError(t, err)
 		})
 	}
+}
+
+func TestServer_GetOrders(t *testing.T) {
+	orderTime, err := time.Parse(time.RFC3339, "2022-04-30T20:00:00+03:00")
+	require.NoError(t, err)
+	orders := []entities.OrderModel{
+		{
+			OrderNum:   "12345674",
+			Login:      "hello",
+			UploadedAt: orderTime,
+			Status:     "NEW",
+		},
+		{
+			OrderNum:   "12345678903",
+			Login:      "hello",
+			UploadedAt: orderTime,
+			Status:     "PROCESSING",
+		},
+		{
+			OrderNum:   "9278923470",
+			Login:      "hello",
+			UploadedAt: orderTime,
+			Status:     "PROCESSED",
+			Accrual:    sql.NullFloat64{Float64: 500, Valid: true},
+		},
+		{
+			OrderNum:   "346436439",
+			Login:      "hello",
+			UploadedAt: orderTime,
+			Status:     "INVALID",
+			Accrual:    sql.NullFloat64{Float64: 500, Valid: true},
+		},
+	}
+	var expected []entities.OrderResponse
+	for _, o := range orders {
+		var accrual *float64 = nil
+		if o.Accrual.Valid {
+			accrual = &(o.Accrual.Float64)
+		}
+		expected = append(expected, entities.OrderResponse{
+			OrderNum:   o.OrderNum,
+			UploadedAt: o.UploadedAt.Format(time.RFC3339),
+			Status:     o.Status,
+			Accrual:    accrual,
+		})
+	}
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	repo := mocks.NewMockStorage(mockCtrl)
+	repo.EXPECT().Init(gomock.Any())
+	repo.EXPECT().GetUserIfExists(gomock.Any(), gomock.Eq("hello")).Return(
+		&entities.UserModel{Login: "hello", PasswordHash: "123456", Session: "123"}, nil).MinTimes(0)
+	repo.EXPECT().UpdateUserSession(gomock.Any(), gomock.Any()).MinTimes(0)
+	repo.EXPECT().GetUserBySessionIfExists(gomock.Any(), gomock.Any()).Return(
+		&entities.UserSessionModel{Login: "hello", Session: "123"}, nil).MinTimes(0)
+
+	repo.EXPECT().GetOrdersForUser(gomock.Any(), "hello").Return(
+		orders, nil).MinTimes(0)
+
+	server := NewGothServer(repo, "")
+	body, err := json.Marshal(entities.LoginRequest{Login: "hello", Password: "123456"})
+	require.NoError(t, err)
+	request := httptest.NewRequest(http.MethodPost, "/api/user/login", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+	h := server.ServerHandler()
+	h.ServeHTTP(w, request)
+	result := w.Result()
+	cookie := http.Cookie{}
+	if len(result.Cookies()) > 0 {
+		auth := result.Cookies()[0]
+		cookie = http.Cookie{Name: auth.Name, Value: auth.Value}
+	}
+
+	err = result.Body.Close()
+	require.NoError(t, err)
+
+	request = httptest.NewRequest(http.MethodGet, "/api/user/orders", bytes.NewBuffer(nil))
+	request.AddCookie(&cookie)
+	w = httptest.NewRecorder()
+	h = server.ServerHandler()
+	h.ServeHTTP(w, request)
+	result = w.Result()
+
+	assert.Equal(t, 200, result.StatusCode)
+
+	ordersResult, err := ioutil.ReadAll(result.Body)
+	require.NoError(t, err)
+	var actualOrders []entities.OrderResponse
+	err = json.Unmarshal(ordersResult, &actualOrders)
+	require.NoError(t, err)
+	assert.ElementsMatch(t, actualOrders, expected)
+
+	err = result.Body.Close()
+	require.NoError(t, err)
 }
